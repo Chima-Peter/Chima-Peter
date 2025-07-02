315c5ee8cde615542a2abbdfe9ba9d3bfda65016
Mobi <chimaojimma@gmail.com>
2025-07-02 23:26:17 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <main>
+      <h1>Welcome to the Page</h1>
+      <p>This is a simple Page component.</p>
+    </main>
+  )
+}
+
+export default Page
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sen: "openSans(--open-sans)",
+      }
+    },
+    screens: {
+      xs: '360px', // Small phones (iPhone SE, Galaxy A series)
+      sm: '640px', // Standard phones
+      md: '768px', // Tablets
+      lg: '1024px', // Laptops
+      xl: '1440px', // Larger desktops
+      '2xl': '2560px', // Very large desktops
+    },
+  },
+  plugins: [],
+}
