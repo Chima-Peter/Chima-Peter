e36fb0b1725d995b680c578099efaf62df891f32
Mobi <93464955+Chima-Peter@users.noreply.github.com>
2025-07-02 23:38:00 +0100

    enable vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Sen } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
+
 
 const sen = Sen({
   variable: "--font-sen",
@@ -22,6 +24,7 @@ export default function RootLayout({
       <body className={`${sen.variable}`}>
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
